7909f8f5a83eaedf81e1314170f6560464586fce
Richard Herries <richardherries@gmail.com>
2012-02-19 20:33:18 +0100

    css changes

--- config.rb
+++ config.rb
@@ -6,7 +6,7 @@ css_dir = "/gfx/"
 sass_dir = "/gfx/sass"
 javascripts_dir = "/js/"
 images_dir = "/gfx/"
-output_style = :compressed
+output_style = :compact
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
@@ -15,7 +15,7 @@ output_style = :compressed
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
